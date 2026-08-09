7559575f8b264a0c445b96113c658dcb61624718
poliwhirlge <141366895+poliwhirlge@users.noreply.github.com>
2026-08-09 03:48:07 -0400

    Add gradient textures for FCs, start to script score display prefab.

---
+++ Assets/Script/Menu/MusicLibrary/MoreInfoMenuScoreDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+using YARG.Core;
+using YARG.Core.Input;
+using YARG.Core.Song;
+using YARG.Core.Utility;
+using YARG.Helpers.Extensions;
+using YARG.Menu.Navigation;
+using YARG.Menu.Persistent;
+using YARG.Scores;
+using YARG.Song;
+using static System.Globalization.CultureInfo;
+
+namespace YARG.Menu.MusicLibrary
+{
+    public class MoreInfoMenuScoreDisplay : MonoBehaviour
+    {
+        [SerializeField]
+        private Image _difficultyIcon;
+        [SerializeField]
+        private TextMeshProUGUI _playerName;
+        [SerializeField]
+        private TextMeshProUGUI _scoreText;
+        [SerializeField]
+        private TextMeshProUGUI _percentText;
+        [SerializeField]
+        private StarView _starView;
+
+    }
+}
